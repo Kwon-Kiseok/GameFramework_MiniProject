52a0ea30e6114f0919fb03c32274343f05444e40
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-10-05 02:56:15 +0900

    10.05 60 프레임 제한

--- GameFramework_MiniProject/main.cpp
+++ GameFramework_MiniProject/main.cpp
@@ -4,15 +4,33 @@ Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	//프레임 고정
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+	
 	game = new Game();
 
 	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		800, 600, false);
 
 	while (game->running()) {
+
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		//프레임 딜레이를 주는 부분
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
