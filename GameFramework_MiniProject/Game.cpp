97ab0896df35a0b32baa57c9ee62bb66223a78ba
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-10-05 03:38:48 +0900

    10.05 게임 오브젝트 클래스 구성

--- GameFramework_MiniProject/Game.cpp
+++ GameFramework_MiniProject/Game.cpp
@@ -4,9 +4,10 @@
 */
 #include "Game.h"
 #include "TextureManager.h"
+#include "GameObject.h"
 
-SDL_Texture* playerTex;
-SDL_Rect srcR, destR;
+GameObject *player;
+GameObject *enemy;
 
 Game::Game()
 {}
@@ -44,7 +45,9 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
 	}
 
 	//플레이어 텍스쳐를 텍스쳐매니저로 로드해줌
-	playerTex = TextureManager::LoadTexture("assets/Player.png", renderer);
+	//playerTex = TextureManager::LoadTexture("assets/Player.png", renderer);
+	player = new GameObject("assets/Player.png", renderer,0,0);
+	enemy = new GameObject("assets/Enemy.png", renderer, 50, 50);
 }
 
 void Game::handleEvents()
@@ -63,18 +66,15 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	cnt++;
-	destR.h = 64;
-	destR.w = 64;
-	destR.x = cnt;
-
-	std::cout << cnt << std::endl;
+	player->Update();
+	enemy->Update();
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+	player->Render();
+	enemy->Render();
 	SDL_RenderPresent(renderer);
 }
 
---
+++ GameFramework_MiniProject/GameObject.h
@@ -0,0 +1,27 @@
+/*
+	2018.10.05 Kwon ki seok
+	GameObject.h
+	게임 오브젝트 클래스
+*/
+#pragma once
+#include "Game.h"
+
+class GameObject {
+
+public:
+	GameObject(const char* tecturesheet, SDL_Renderer* ren, int x, int y);
+	~GameObject();
+
+	void Update();
+	void Render();
+
+private:
+
+	int xpos;
+	int ypos;
+
+	SDL_Texture* objTexture;
+	SDL_Rect srcRect, destRect;
+	SDL_Renderer* renderer;
+
+};
